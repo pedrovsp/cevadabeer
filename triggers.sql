cef7054fe497ee3149194c3db020f5d68f46bb0f
pedrovspereira <pedrovitorino.s.p@gmail.com>
2019-06-16 18:49:17 -0300

    procs criadas, acento removido de saida

--- triggers.sql
+++ triggers.sql
@@ -1,7 +1,8 @@
 use CevadaBeer;
 
--- before insert movimentacao atualizar estoque
-CREATE OR REPLACE TRIGGER before_movimentacao_insert
+-- Quando há entrada ou saida no estoque é atualizado a quantidade de cada produto
+DELIMITER $$
+CREATE TRIGGER before_movimentacao_insert
     BEFORE INSERT ON MovimentacaoEstoque
     FOR EACH ROW
 BEGIN
@@ -12,7 +13,7 @@ BEGIN
 		UPDATE Produto
 		SET Quantidade = qtd_atual + new.Quantidade
 		where Id = new.fk_Produto_Id;
-	ELSE IF NEW.Tipo = 'Sa�da' then
+	ELSE IF NEW.Tipo = 'Saida' then
 		UPDATE Produto
 		SET Quantidade = (qtd_atual - new.Quantidade)
 		where Id = new.fk_Produto_Id;
@@ -20,30 +21,31 @@ BEGIN
 	END if;
 end;
 
-insert into cevadabeer.movimentacaoestoque(Data_Registro, Tipo, Quantidade, fk_Produto_Id) values ('2019-10-07', "Entrada", 100, 1);
+-- insert into MovimentacaoEstoque(Data_Registro, Tipo, Quantidade, fk_Produto_Id) values ('2019-10-07', "Entrada", 100, 1);
 
--- before update finalizar producao atualizar MovimentacaoEstoque
--- !!!!!!!!!!!!!!! ERRO !!!!!!!!!!!!! --
-CREATE OR REPLACE TRIGGER before_producao_update
-    after update ON producao
+-- Ao finalizar producao insere a entrada do produto produzido no estoque
+DELIMITER $$
+CREATE TRIGGER before_producao_update
+    after update ON Producao
     FOR EACH ROW
 BEGIN
 	declare qtd_garrafas integer;
-	if new.Dt_Fim <> null then
+	if new.Dt_Fim is not null then
 		set qtd_garrafas = (new.quantidade * 1000) / 600;
 	
-		INSERT INTO cevadabeer.movimentacaoestoque(Data_Registro, Tipo, Quantidade, fk_Produto_Id)
+		INSERT INTO MovimentacaoEstoque(Data_Registro, Tipo, Quantidade, fk_Produto_Id)
 		VALUES
 		(curdate(), "Entrada", qtd_garrafas, 1);
 	
 	END if;
 end;
 
-update cevadabeer.producao set `Dt_Fim` = curdate() where `Id` = 2;
+-- update Producao set `Dt_Fim` = curdate() where `Id` = 2;
 
--- before insert on producao verificar igredientes
-CREATE OR REPLACE TRIGGER before_producao_insert
-    BEFORE INSERT ON producao
+-- Verifica se há a igredientes suficientes para iniciar a producao.
+DELIMITER $$
+CREATE TRIGGER before_producao_insert
+    BEFORE INSERT ON Producao
     FOR EACH ROW
 begin
     DECLARE done INT DEFAULT FALSE;
@@ -52,8 +54,8 @@ begin
    	declare qtd decimal;
    	declare qtd_litro decimal;
    	declare qtd_total decimal;
-    DECLARE cur CURSOR for select ingredientes.Id, ingredientes.QuantidadeEstoque, contem.QtdPorLitro from ingredientes, cerveja, contem where
-	ingredientes.Id = contem.fk_Ingredientes_Id and cerveja.Id = contem.fk_Cerveja_Id and cerveja.Id = new.fk_Cerveja_Id;
+    DECLARE cur CURSOR for select Ingredientes.Id, Ingredientes.QuantidadeEstoque, Contem.QtdPorLitro from Ingredientes, Cerveja, Contem where
+	ingredientes.Id = Contem.fk_Ingredientes_Id and Cerveja.Id = Contem.fk_Cerveja_Id and cerveja.Id = new.fk_Cerveja_Id;
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
     OPEN cur;
@@ -65,19 +67,20 @@ begin
             
            	set qtd_total = qtd_litro * new.Quantidade;
            	if (qtd < qtd_total) then
-           		SET	msg	=	'Quantidade de igredientes insuficiente';
-				SIGNAL	SQLSTATE	'45000'	SET	MESSAGE_TEXT	=	msg;
+           		SET	msg	= 'Quantidade de igredientes insuficiente';
+				SIGNAL SQLSTATE '45000'	SET	MESSAGE_TEXT = msg;
 			end if;
         END LOOP;
     CLOSE cur;
 	
 end;
 
-insert into cevadabeer.producao(Dt_Inicio, Quantidade, `fk_Cerveja_Id`) values (curdate(), 10, 1);
+-- insert into Producao(Dt_Inicio, Quantidade, `fk_Cerveja_Id`) values (curdate(), 10, 1);
 
--- before atualizar senha verificar se sao diferentes
-CREATE OR REPLACE TRIGGER before_usuario_update
-    before update ON usuario
+-- Verifica se o usuario esta entrado com uma senha nao repetida ao trocar de senha
+DELIMITER $$
+CREATE TRIGGER before_usuario_update
+    before update ON Usuario
     FOR EACH ROW
 begin
 	declare msg varchar(255);
@@ -87,11 +90,12 @@ begin
 	END if;
 end;
 
-update cevadabeer.usuario set `Senha` = '2222' where `Id` = 1;
+-- update cevadabeer.usuario set `Senha` = '2222' where `Id` = 1;
 
--- 
-CREATE OR REPLACE TRIGGER after_producao_insert
-    after INSERT ON producao
+-- Ao iniciar producao retira os ingredientes utilizados do estoque
+DELIMITER $$
+CREATE TRIGGER after_producao_insert
+    after INSERT ON Producao
     FOR EACH ROW
 begin
     DECLARE done INT DEFAULT FALSE;
@@ -101,8 +105,8 @@ begin
    	declare qtd_litro decimal;
    	declare qtd_total decimal;
     declare nova_qtd decimal;
-    DECLARE cur CURSOR for select ingredientes.Id, ingredientes.QuantidadeEstoque, contem.QtdPorLitro from ingredientes, cerveja, contem where
-	ingredientes.Id = contem.fk_Ingredientes_Id and cerveja.Id = contem.fk_Cerveja_Id and cerveja.Id = new.fk_Cerveja_Id;
+    DECLARE cur CURSOR for select Ingredientes.Id, Ingredientes.QuantidadeEstoque, Contem.QtdPorLitro from Ingredientes, Cerveja, Contem where
+	Ingredientes.Id = Contem.fk_Ingredientes_Id and Cerveja.Id = Contem.fk_Cerveja_Id and Cerveja.Id = new.fk_Cerveja_Id;
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
     OPEN cur;
@@ -120,4 +124,4 @@ begin
 	
 end;
 
-insert into cevadabeer.producao(Dt_Inicio, Quantidade, `fk_Cerveja_Id`) values (curdate(), 10, 1);
+-- insert into Producao(Dt_Inicio, Quantidade, `fk_Cerveja_Id`) values (curdate(), 10, 1);
